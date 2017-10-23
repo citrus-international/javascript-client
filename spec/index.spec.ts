c2b3ebbbe95b2a28c517f8b12a826df2235b20a3
Jake Mc <jake@citrusad.com>
2017-10-23 13:15:02 +1000

    We now e2e test against the build asset

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,23 +1,25 @@
-import { CitrusAd } from '../src/CitrusAd';
+// We use the dist file, make sure you build before running the test
+// import { CitrusAd } from '../src/CitrusAd';
+declare var CitrusAd;
 
-const DEV_ENDPOINT = 'https://dev-integration.citrusad.com/v1';
+const DEV_ENDPOINT = 'https://staging-integration.citrusad.com/v1';
 
 describe('Test suit for CitrusAd JS Library', () => {
 
-  const TEAM_ID = 'd9fb0607-6f5b-4468-8c1b-89bbcc9a5e65';
-  const RANDOM_AD_ID = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+  const RANDOM_AD_ID = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
+
   describe('can report', () => {
-    let citrusAd: CitrusAd = null;
+    let citrusAd: any = null;
     beforeEach(() => {
       citrusAd = CitrusAd.init({ overrideApiAddress: DEV_ENDPOINT });
     });
 
     it('impression', () => {
-      return citrusAd.reportImpression(RANDOM_AD_ID, TEAM_ID);
+      return citrusAd.reportImpression(RANDOM_AD_ID);
     });
 
     it('click', () => {
-      return citrusAd.reportClick(RANDOM_AD_ID, TEAM_ID);
+      return citrusAd.reportClick(RANDOM_AD_ID);
     });
 
   });
--- test/CitrusAd.test.ts
+++ test/CitrusAd.test.ts
@@ -1,24 +1,23 @@
 import * as sinon from 'sinon';
 import { CitrusAd } from '../src/CitrusAd';
 
-describe('Test suit for CitrusAd JS Library', function () {
+describe('Test suit for CitrusAd JS Library', () => {
 
   describe('_executeAction', () => {
 
     let mockFailingAction: any;
     let citrusAd: CitrusAd = null;
     beforeEach(() => {
-      const mockJWTProvider = sinon.stub().returns(Promise.resolve('jwt'));
       mockFailingAction = sinon.stub().returns(Promise.reject({ status: 500 }));
-      citrusAd = CitrusAd.init(mockJWTProvider, 'https://endpoint');
+      citrusAd = CitrusAd.init({ overrideApiAddress: 'https://endpoint' });
     });
 
     it('retries actions three times', (done) => {
       return citrusAd['_executeAction'](mockFailingAction, 0)
-      .catch((error) => {
-        sinon.assert.calledThrice(mockFailingAction);
-        done();
-      });
+        .catch((error) => {
+          sinon.assert.calledThrice(mockFailingAction);
+          done();
+        });
     });
 
   });
